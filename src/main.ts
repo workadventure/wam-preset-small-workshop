547bf0d674aa1b1b21684cb07774119e942b372a
Valdo <vteixeiraromao@gmail.com>
2023-04-29 16:23:28 +0100

    tmj file, fixed script, png path, removed collisions

--- src/main.ts
+++ src/main.ts
@@ -4,53 +4,10 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentZone: string;
-let currentPopup: any;
-const config = [
-    {
-        zone: 'needHelp',
-        message: 'Do you need some guidance? We are happy to help you out.',
-        cta: [
-            {
-                label: 'Meet us',
-                className: 'primary',
-                callback: () => WA.openTab('https://play.staging.workadventu.re/@/tcm/workadventure/wa-village'),
-            }
-        ]
-    },
-    {
-        zone: 'followUs',
-        message: 'Hey! Have you already started following us?',
-        cta: [
-            {
-                label: 'LinkedIn',
-                className: 'primary',
-                callback: () => WA.openTab('https://www.linkedin.com/company/workadventu-re'),
-            },
-            {
-                label: 'Twitter',
-                className: 'primary',
-                callback: () => WA.openTab('https://twitter.com/workadventure_'),
-            }
-        ]
-    },
-]
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
-    
-    WA.onEnterZone('needHelp', () => {
-        currentZone = 'needHelp'
-        openPopup(currentZone, currentZone + 'Popup')
-    });
-    WA.onEnterZone('followUs', () => {
-        currentZone = 'followUs'
-        openPopup(currentZone, currentZone + 'Popup')
-    });
-    WA.onLeaveZone('needHelp', closePopup);
-    WA.onLeaveZone('followUs', closePopup);
 
     // Show configuration tile for editors only
     if (WA.player.tags.includes('editor')) {
@@ -66,21 +23,5 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
-
-function openPopup(zoneName: string, popupName: string) {
-    const zone = config.find((item) => {
-        return item.zone == zoneName
-    });
-    if (typeof zone !== 'undefined') {
-        // @ts-ignore otherwise we can't use zone.cta object
-        currentPopup = WA.openPopup(popupName, zone.message, zone.cta)
-    }
-}
-function closePopup(){
-    if (typeof currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 export {};
